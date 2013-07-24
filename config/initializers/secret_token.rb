8b0c78599c3638ed64c2fd87b889b65be09c55a4
Meeka Gayhart <rrgayhart@gmail.com>
2013-07-24 10:27:00 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blawgger2::Application.config.secret_key_base = '57105f86333d3760c989ab9bc671317673b13d2c464cd61c22d4532ff4ce40d74f752baeb75e054fbba899808a36a9b5a50e7b8ecd1355fd764c6851110697e3'
